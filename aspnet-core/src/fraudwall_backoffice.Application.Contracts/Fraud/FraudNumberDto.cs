d5ef1db774d0e9676a81f48dc3e1b1ea66e0d9cc
ericnanaosei <eric.nana-osei@afrilogicsolutions.com>
2022-09-08 09:23:34 +0000

    fraud number application services

---
+++ aspnet-core/src/fraudwall_backoffice.Application.Contracts/Fraud/FraudNumberDto.cs
@@ -0,0 +1,14 @@
+using System;
+using fraudwall_backoffice.Risk;
+using Volo.Abp.Application.Dtos;
+
+namespace fraudwall_backoffice.Fraud;
+
+public class FraudNumberDto: AuditedEntityDto<Guid>
+{
+  public string PhoneNumber { get; set; }
+  public int ReportCount { get; set; }
+  public float Rating { get; set; }
+  public RiskType RiskLevel { get; set; }
+
+}
