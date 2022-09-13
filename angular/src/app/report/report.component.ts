ab16d36f73354f69ecde4da64245cd20c251143c
ericnanaosei <eric.nana-osei@afrilogicsolutions.com>
2022-09-13 18:09:51 +0000

    report service created

--- angular/src/app/report/report.component.ts
+++ angular/src/app/report/report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportService } from './report.service';
 
 @Component({
   selector: 'app-report',
@@ -6,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  constructor( private readonly reportService: ReportService) { }
+  report$: [];
 
   ngOnInit(): void {
+    this.reportService.getListReport().subscribe( reports => {
+      this.report$ = reports;
+    });
   }
 
+  getReport(id: string){
+    this.reportService.getReport(id).subscribe(report => {
+      return report;
+    });
+  }
 }
