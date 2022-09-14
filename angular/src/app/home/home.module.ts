504fea495655b3701adcbf72648152b8c0d62706
ericnanaosei <eric.nana-osei@afrilogicsolutions.com>
2022-09-14 10:57:15 +0000

    getting total report on card

--- angular/src/app/home/home.module.ts
+++ angular/src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CardModule } from '../card/card.module';
+import { ReportService } from '../report/report.service';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -7,6 +8,7 @@ import { HomeComponent } from './home.component';
 @NgModule({
   declarations: [HomeComponent],
   imports: [SharedModule, HomeRoutingModule, CardModule],
+  providers: [ReportService],
   exports: [HomeComponent],
 })
 export class HomeModule {}
