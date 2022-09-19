ad52620a292f0ea1d39b6771d24cdd5579c981cc
ericnanaosei <eric.nana-osei@afrilogicsolutions.com>
2022-09-19 16:52:53 +0000

    investigation list table

--- angular/src/app/investigation/investigation.component.ts
+++ angular/src/app/investigation/investigation.component.ts
@@ -1,14 +1,34 @@
+
+import { ListService, PagedResultDto } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
+import { ReportInvestigationService, InvestigationReportDto } from '@proxy/investigation';
 
 @Component({
   selector: 'app-investigation',
   templateUrl: './investigation.component.html',
+  providers: [ListService],
 })
 export class InvestigationComponent implements OnInit {
+  investigations = { items: [], totalCount: 0 } as PagedResultDto<InvestigationReportDto>;
+  page: number = 0;
 
-  constructor() { }
+  constructor(public readonly list: ListService, private reportInvestigation: ReportInvestigationService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getInvestigation();
+  }
+  
+  // get Investigation
+  getInvestigation():any{
+    const investigationStreamCreator = (query) => this.reportInvestigation.getList(query);
+
+    return this.list.hookToQuery(investigationStreamCreator).subscribe((response) => {
+      this.investigations = response;
+    });
   }
 
+  pageChangeEvent(event: number){
+    this.page = event;
+    this.investigations = this.getInvestigation();
+  }
 }
