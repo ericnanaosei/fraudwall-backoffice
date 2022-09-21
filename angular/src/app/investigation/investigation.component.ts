106ea50ba7d2a6dd4a29e1d8d20ba11d37444a38
ericnanaosei <eric.nana-osei@afrilogicsolutions.com>
2022-09-21 11:28:06 +0000

    create investigation form added

--- angular/src/app/investigation/investigation.component.ts
+++ angular/src/app/investigation/investigation.component.ts
@@ -1,7 +1,8 @@
 
 import { ListService, PagedResultDto } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
-import { ReportInvestigationService, InvestigationReportDto } from '@proxy/investigation';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReportInvestigationService, InvestigationReportDto, statusOptions} from '@proxy/investigation';
 
 @Component({
   selector: 'app-investigation',
@@ -11,8 +12,15 @@ import { ReportInvestigationService, InvestigationReportDto } from '@proxy/inves
 export class InvestigationComponent implements OnInit {
   investigations = { items: [], totalCount: 0 } as PagedResultDto<InvestigationReportDto>;
   page: number = 0;
+  isModalOpen = false;
+  investigationForm: FormGroup;
+  investigationStatusOptions = statusOptions;
 
-  constructor(public readonly list: ListService, private reportInvestigation: ReportInvestigationService) {}
+  constructor(
+    public readonly list: ListService, 
+    private investigationService: ReportInvestigationService,
+    private readonly formBuilder: FormBuilder
+    ) {}
 
   ngOnInit() {
     this.getInvestigation();
@@ -20,7 +28,7 @@ export class InvestigationComponent implements OnInit {
   
   // get Investigation
   getInvestigation():any{
-    const investigationStreamCreator = (query) => this.reportInvestigation.getList(query);
+    const investigationStreamCreator = (query) => this.investigationService.getList(query);
 
     return this.list.hookToQuery(investigationStreamCreator).subscribe((response) => {
       this.investigations = response;
@@ -30,5 +38,33 @@ export class InvestigationComponent implements OnInit {
   pageChangeEvent(event: number){
     this.page = event;
     this.investigations = this.getInvestigation();
+  }
+
+  // create Investigation
+  createInvestigation(){
+    this.buildForm();
+    this.isModalOpen = true;
+  }
+
+  // build form
+  buildForm(){
+    this.investigationForm = this.formBuilder.group({
+      reportId: [null, Validators.required],
+      investigationStatus: [null, Validators.required],
+      assignedUserId: [null, Validators.required]
+    });
+  }
+
+  // add save method
+  save() {
+    if (this.investigationForm) {
+      return;
+    }
+
+    this.investigationService.create(this.investigationForm.value).subscribe(() => {
+      this.isModalOpen = false;
+      this.investigationForm.reset();
+      this.list.get();
+    });
   }
 }
